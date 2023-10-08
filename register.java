71a39e274fed263420e9ee4932be1f7ae545f976
Vishvaa08 <86644867+Vishvaa08@users.noreply.github.com>
2023-10-08 23:19:44 +0800

    RegisterHelperClass and Register
    Updated register file, now works with database

--- register.java
+++ register.java
@@ -1,18 +1,27 @@
-package com.example.bloodunitytest;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.test2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class register extends AppCompatActivity {
+
+    EditText firstName, lastName, userEmail, userPassword, userCPassword, userAddress;
+    Spinner bloodType;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +37,8 @@ public class register extends AppCompatActivity {
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(register.this, "Back", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(register.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -48,22 +58,88 @@ public class register extends AppCompatActivity {
         //add adapter to the spinner (line 47)
         spinner.setAdapter(adapter);
 
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        userEmail = findViewById(R.id.user_email);
+        userPassword = findViewById(R.id.user_password);
+        userCPassword = findViewById(R.id.user_cpassword);
+        userAddress = findViewById(R.id.user_address);
+        bloodType = findViewById(R.id.blood_type);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("users");
+
         Button btnRegister = (Button) findViewById(R.id.register_button);
         Button btnLogin = (Button) findViewById(R.id.login_button);
 
         //onclick for when login button is pressed
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                validateRegisterForm();
+
             }
         });
+
     }
 
-    public void Register(View view){
-        Intent intent = new Intent(register.this, shop.class);
-        Button shop = (Button) findViewById(R.id.register_button);
-        startActivity(intent);
+    private Boolean validateRegisterForm(){
+
+        String valueFirstName = firstName.getText().toString();
+        String valueLastName   = lastName.getText().toString();
+        String valueEmail  = userEmail.getText().toString();
+        String valuePassword  = userPassword.getText().toString();
+        String valueCPassword = userCPassword.getText().toString();
+        String passwordValidation = "^" + "(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[0-9])" + "(?=.*[@#$%^&+=])" + ".{6,}" + "$";
+        String valueAddress  = userAddress.getText().toString();
+        String blood = bloodType.getSelectedItem().toString();
+
+        if (valueFirstName.isEmpty()) {
+            firstName.setError("Field cannot be empty");
+            return false;
+        }else if (valueLastName.isEmpty()){
+            lastName.setError("Field cannot be empty");
+            return false;
+        }else if (valueEmail.isEmpty()){
+            userEmail.setError("Field cannot be empty");
+            return false;
+        }else if (valuePassword.isEmpty()) {
+            userPassword.setError("Field cannot be empty");
+            return false;
+        }else if (!valuePassword.matches(passwordValidation)) {
+            userPassword.setError("Password is too weak");
+            return false;
+        }else if (valueCPassword.isEmpty()){
+            userCPassword.setError("Field cannot be empty");
+            return false;
+        }else if (valueAddress.isEmpty()){
+            userAddress.setError("Field cannot be empty");
+            return false;
+        }else{
+            firstName.setError(null);
+            lastName.setError(null);
+            userEmail.setError(null);
+            userPassword.setError(null);
+            userAddress.setError(null);
+            insertUserData();
+            return true;
+        }
+    }
+
+    //String email pattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    private void insertUserData(){
+        String fname = firstName.getText().toString();
+        String lname = lastName.getText().toString();
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+        String address = userAddress.getText().toString();
+        String blood = bloodType.getSelectedItem().toString();
+
+        RegisterHelperClass registerHelperClass = new RegisterHelperClass(fname, lname, email, password, address, blood);
+
+        reference.push().setValue(registerHelperClass);
+        Toast.makeText(register.this, "Successful Register!", Toast.LENGTH_SHORT).show();
     }
 
     }
